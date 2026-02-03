6dba2670b8ef7ae5abb022a8a97ddedc048f25d4
Sonu parsad <sonusa470@gmail.com>
2026-02-03 11:42:38 +0000

    hi

---
+++ C/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello Sonu, C is running in Codespace 🚀\n");
+    return 0;
+}
---
+++ CPP/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello Sonu, C++ is running 🚀" << endl;
+    return 0;
+}
+
